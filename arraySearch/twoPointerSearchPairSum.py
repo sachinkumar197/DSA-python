88b5c5110218f50afc1208b0f2435b1b09d17a47
sachin <sachin.kumar@vayana.com>
2022-11-19 15:53:06 +0530

    using two pointers to find pair with reqd sum

---
+++ arraySearch/twoPointerSearchPairSum.py
@@ -0,0 +1,16 @@
+# https://www.geeksforgeeks.org/given-an-array-a-and-a-number-x-check-for-pair-in-a-with-sum-as-x/?ref=lbp
+
+def searchElem(xs, left, right, val):
+    xs.sort()
+    if xs[left] + xs[right] == val:
+        return "Yes"
+    elif xs[left] + xs[right] > val:
+        return searchElem(xs, left, right - 1, val)
+    elif xs[left] + xs[right] < val:
+        return searchElem(xs, left - 1, right, val)
+    return "No"
+
+
+A = [0, -1, 2, -3, 1]
+x = -2
+print(searchElem(A, 0, len(A)-1, x))
